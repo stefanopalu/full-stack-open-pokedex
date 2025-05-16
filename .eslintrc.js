a3a3e806062496d0cdd3d4b48326bca7ee41fa88
stefanopalu <stefanizzimail@hotmail.it>
2025-05-16 12:58:43 +0900

    try3

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
